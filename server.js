a22c88df8c10ecc345a3c0942b1abb196d91aa63
Ismael <ideidao11@gmail.com>
2025-10-24 22:50:04 -0400

    Made some changes for Heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 //Initializing app
 app.use(express.urlencoded({extended: true}));
