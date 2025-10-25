1c38238cbf7c5a9a6b9378f1b40294d547dd1e68
Ismael <ideidao11@gmail.com>
2025-10-24 22:11:18 -0400

    First commit of project

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 4000;
+
+//Initializing app
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+//Root route
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+})
+
+//Joke route
+app.get('/joke', async (req, res) =>{
+    try {
+        const response = await fetch("https://icanhazdadjoke.com/", {headers:{Accept: 'text/plain'}});
+        let joke = await response.text(); //Converts promise into actual data
+
+        console.log("Code 201: Joke created successfully");
+        res.status(201).send(joke);
+
+    } catch (error) {
+        res.status(500).send("Error on joke call!");
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
